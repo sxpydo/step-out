df1336f4fe119139e7a63fa04f63e88f93f7f4fc
sxpydo <shavypeynado@gmail.com>
2026-06-07 19:31:13 +0100

    step-out(fix): fix dashboard image

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,31 +1,30 @@
 import React from "react";
 import ChatBot from "./features/chatterbot/Chatbot";
 import DashNav from "./navs/DashNav";
+import homeImg from "../images/home-image.png";
 
 const Dashboard = (props) => {
+  console.log(props);
 
-console.log(props)
-
-    return(
-        <>
-        <h1>{`Welcome ${props.user.first_name}!`}</h1>
-        <div className="flexible-layout">
+  return (
+    <>
+      <h1>{`Welcome ${props.user.first_name}!`}</h1>
+      <div className="flexible-layout">
         <div className="image-layout">
-        <p>“At work at play, let safety lead the way”</p>
-            <img
-            src="https://blush.design/api/download?shareUri=otuoM_8wLg0KiKiw&w=800&h=800&fm=png"
-            alt=" Illustration by Blush" className="dash-img"/>
-            </div>
+          <p>“At work at play, let safety lead the way”</p>
+          <img src={homeImg} alt="Illustration by Blush" className="home-img" />
+        </div>
         <div className="chatbot">
-            <ChatBot />
-            <p>Your safety net when guardians are busy or needing guidance in using
-                the Step Out app!
-            </p>
+          <ChatBot />
+          <p>
+            Your safety net when guardians are busy or needing guidance in using
+            the Step Out app!
+          </p>
         </div>
-        </div>
-        <DashNav />
-        </>
-    )
-}
+      </div>
+      <DashNav />
+    </>
+  );
+};
 
 export default Dashboard;
